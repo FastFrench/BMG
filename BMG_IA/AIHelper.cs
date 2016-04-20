fb5642f316c879e7d9ee8bff2454d2cd39204879
FastFrench <antispam@laposte.net>
2016-04-20 19:08:23 +0200

    Files forgotten

---
+++ BMG_IA/AIHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMG_Structures;
+using BMG_Structures.Common;
+using BMG_Structures.Troops;
+
+namespace BMG_IA
+{
+	static public class AIHelper
+	{
+
+	}
+}
---
+++ BMG_Structures/Common/CommonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMG_Structures.Troops;
+
+namespace BMG_Structures.Common
+{
+	static public class CommonHelper
+	{
+		static public CellBase.CellContent ImpossibleMoveMask(this AltitudeEnum altitude)
+		{
+			CellBase.CellContent mask = CellBase.CellContent.TotalObstacles;
+			if (!altitude.HasFlag(AltitudeEnum.Fly)) mask ^= CellBase.CellContent.FlyObstacle;
+			if (!altitude.HasFlag(AltitudeEnum.Ground)) mask ^= CellBase.CellContent.GroundObstacle;
+			if (!altitude.HasFlag(AltitudeEnum.Underground)) mask ^= CellBase.CellContent.UnderGroundObstacle;
+			return mask;
+		}
+		static public CellBase.CellContent ImpossibleMoveMask(this TroopBase troop)
+		{
+			return troop.Altitude.ImpossibleMoveMask();
+		}
+		static public CellBase.CellContent TargetMask(this PlaceableBase placeable)
+		{
+			return placeable.TargetableAltitudes.ImpossibleMoveMask();
+		}
+
+	}
+}
