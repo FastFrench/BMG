f7e1216065602d118b464358c7f234fd8154d53d
FastFrench <antispam@laposte.net>
2016-04-25 11:35:14 +0200

    Lissing files

---
+++ BMG_Structures/Common/TemplateBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMG_Structures.Common
+{
+	public class TemplateBase
+	{
+		public int TemplateId { get; set; }
+		public string Name { get; set; }
+
+		public int Cost { get; set; }
+		public int Delay { get; set; }
+	
+		public int MaxHP { get; set; }
+		public int Range { get; set; }
+
+		public int Damage { get; set; }
+		public AltitudeEnum Altitude { get; set; }
+		public AltitudeEnum TargetableAltitude { get; set; }
+	}
+}
---
+++ BMG_UnitTest/Common/TestBase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using BMG_Structures;
+using BMG_Structures.Common;
+using BMG_Structures.Troops;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BMG_UnitTest
+{
+	public class TestBase
+	{
+		protected PlayerBase p11, p12, p21, p22;
+		protected TeamBase team1, team2;
+		protected TroopTemplateBase barbTemplate;
+		protected TroopTemplateBase flyTemplate;
+		protected BattleFieldBase bf;
+
+		protected void Init()
+		{
+			team1 = new TeamBase();
+			team2 = new TeamBase();
+			p11 = team1.AddPlayer("Test1_1", "Password");
+			p12 = team1.AddPlayer("Test1_2", "Password");
+			p21 = team2.AddPlayer("Test2_1", "Password");
+			p22 = team2.AddPlayer("Test2_2", "Password");
+
+			bf = new BattleFieldBase();
+			bf.Initialize(8, 8);
+
+			barbTemplate = new TroopTemplateBase();
+			barbTemplate.Name = "Barbarian";
+			barbTemplate.Range = 2;
+			barbTemplate.MaxHP = 100;
+			barbTemplate.SpaceUsed = 3;
+			barbTemplate.Speed = 4;
+			barbTemplate.TemplateId = 1;
+			barbTemplate.Altitude = AltitudeEnum.Ground;
+
+			flyTemplate = new TroopTemplateBase();
+			flyTemplate.Name = "Fly";
+			flyTemplate.Range = 1;
+			flyTemplate.MaxHP = 20;
+			flyTemplate.SpaceUsed = 1;
+			flyTemplate.Speed = 6;
+			flyTemplate.TemplateId = 2;
+			flyTemplate.Altitude = AltitudeEnum.Fly;
+			
+		}
+
+		protected TroopBase CreateBarbarian(int x, int y, PlayerBase player)
+		{
+			TroopBase barb = new TroopBase(barbTemplate, player);
+			barb.Drop(new Point(x, y));
+			return barb;
+		}
+
+		protected TroopBase CreateFly(int x, int y, PlayerBase player)
+		{
+			TroopBase fly = new TroopBase(flyTemplate, player);
+			fly.Drop(new Point(x, y));
+			return fly;
+		}
+
+
+		public void TestTeams()
+		{
+			Init();
+			var px = team2.AddPlayer("Test1_3", "Password");
+			var px2 = team2["Test1_3"];
+			Assert.IsNotNull(px, "team2.AddPlayer failed");
+			Assert.IsNotNull(px2, "access of a player from team failed");
+			Assert.AreEqual(px.Login, px2.Login);
+			Assert.AreEqual(px.Password, px2.Password);
+			Assert.AreEqual(px, px2);
+			Assert.IsTrue(team1.MoveExistingPlayerIntoThisTeam(px), "MoveExistingPlayerIntoThisTeam failed");
+			var px3 = team2["Test1_3"];
+			Assert.IsNull(px3, "Player still in team2, despite move in team1");
+			var px4 = team1["Test1_3"];
+			Assert.IsNotNull(px3, "Player not found in team1 after moved");
+		}
+
+
+
+		[TestMethod]
+		public void TestMapSearch()
+		{
+			BattleFieldBase bf = new BattleFieldBase();
+			bf.Initialize(8, 8);
+
+			TroopTemplateBase ttb = new TroopTemplateBase();
+			ttb.Name = "Barbarian";
+			ttb.Range = 2;
+			ttb.MaxHP = 100;
+			ttb.SpaceUsed = 1;
+			ttb.Speed = 4;
+			ttb.TemplateId = 1;
+			TroopBase troop = new TroopBase();
+			troop.Drop(new Point(2, 2));
+		}
+
+
+	}
+}
