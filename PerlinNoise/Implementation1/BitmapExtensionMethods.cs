115407eaa09f4dc1cd9b252e71100f9b752e9dc8
FastFrench <antispam@laposte.net>
2016-05-20 12:02:20 +0200

    Perlin Noise searches

---
+++ PerlinNoise/Implementation1/BitmapExtensionMethods.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+
+namespace PerlinNoise.Implementation1
+{
+    public static class BitmapExtensionMethods
+    {
+        public static void ExecuteForEachPixel(this Bitmap bitmap, Action<Point, Bitmap> action)
+        {
+            Point point = new Point(0, 0);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                point.X = x;
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    point.Y = y;
+                    action(point, bitmap);
+                }
+            }
+        }
+
+        public static void ExecuteForEachPixel(this Bitmap bitmap, Action<Point> action)
+        {
+            Point point = new Point(0, 0);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                point.X = x;
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    point.Y = y;
+                    action(point);
+                }
+            }
+        }
+
+        public static void SetEachPixelColour(this Bitmap bitmap, Func<Point, Color> colourFunc)
+        {
+            Point point = new Point(0, 0);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                point.X = x;
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    point.Y = y;
+                    bitmap.SetPixel(x, y, colourFunc(point));
+                }
+            }
+        }
+
+        public static void SetEachPixelColour(this Bitmap bitmap, Func<Point, Color, Color> colourFunc)
+        {
+            Point point = new Point(0, 0);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                point.X = x;
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    point.Y = y;
+                    bitmap.SetPixel(x, y, colourFunc(point, bitmap.GetPixel(x, y)));
+                }
+            }
+        }
+    }
+
+}
+
+/* sample usage:
+ private void renderDistanceFromCenterButton_Click(object sender, EventArgs e)
+{
+    // Build the bitmap
+    Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+    Point imageCenter = new Point(pictureBox.Width / 2, pictureBox.Height / 2);
+ 
+    // Ok, this is terrible, you _really_ shouldn't use lambda like this... But you can...
+    Func<double, double, double> distance = (x, y) => Math.Sqrt(x * x + y * y);
+ 
+    double maxDistance = distance(imageCenter.X, imageCenter.Y);
+    bitmap.SetEachPixelColour(
+        point =>
+        {
+            // Pythagoras
+            double dist = distance((point.X - imageCenter.X), (point.Y - imageCenter.Y));
+            byte b = Convert.ToByte(255 * dist / maxDistance);
+            return Color.FromArgb(b, b, b);
+        });
+    pictureBox.Image = bitmap;
+}
+*/
---
+++ PerlinNoise/Implementation1/Usage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PerlinNoise.Implementation1
+{
+    public class Usage
+    {
+        PictureBox pictureBox=null;
+
+        private void noiseButton_Click(object sender, EventArgs e)
+        {
+            
+            PerlinNoise3D perlinNoise = new PerlinNoise3D(99);
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            double widthDivisor = 1 / (double)pictureBox.Width;
+            double heightDivisor = 1 / (double)pictureBox.Height;
+            bitmap.SetEachPixelColour(
+                (point, color) =>
+                {
+            // Note that the result from the noise function is in the range -1 to 1, but I want it in the range of 0 to 1
+            // that's the reason of the strange code
+            double v =
+                        // First octave
+                        (perlinNoise.Noise(2 * point.X * widthDivisor, 2 * point.Y * heightDivisor, -0.5) + 1) / 2 * 0.7 +
+                        // Second octave
+                        (perlinNoise.Noise(4 * point.X * widthDivisor, 4 * point.Y * heightDivisor, 0) + 1) / 2 * 0.2 +
+                        // Third octave
+                        (perlinNoise.Noise(8 * point.X * widthDivisor, 8 * point.Y * heightDivisor, +0.5) + 1) / 2 * 0.1;
+
+                    v = Math.Min(1, Math.Max(0, v));
+                    byte b = (byte)(v * 255);
+                    return Color.FromArgb(b, b, b);
+                });
+            pictureBox.Image = bitmap;
+        }
+
+    }
+}
